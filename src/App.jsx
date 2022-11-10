5ce9a3fe888e76ec04ca7ac2ebfee377a569fd07
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-11-10 11:36:16 -0300

    en proceso de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Menu from "./components/common/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
 function App() {
   return (
     <div className="App">
-      
+      <Menu></Menu>
     </div>
   );
 }
---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+const Menu = () => {
+    return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="mb-3 navbarPaginas">
+          <Container fluid>
+            <Navbar.Brand href="#">LOGO</Navbar.Brand>
+            <a>Menú</a>
+            <a>Nuestro Café</a>
+            <a>Expieriencia CoderCoffe</a>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton className="navbarExpand">
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Coder Coffe
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Iniciar Sesion</Nav.Link>
+                  <Nav.Link href="#action2">Productos destacados</Nav.Link>
+                  <NavDropdown
+                    title="Menús del dia"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Desayunos/Meriendas</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Almuerzos/cena
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      cerrar
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Buscar Productos"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Buscar</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default Menu;
+
+
