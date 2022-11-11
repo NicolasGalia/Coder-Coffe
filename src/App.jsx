32c0c62d7e2759bbaa1fd3581612f2635286a8f4
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-11-11 11:25:42 -0300

    realizando el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Menu from "./components/common/Menu";
+import Footer from './components/common/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <>
       <Menu></Menu>
+<Footer></Footer>
+      </>
+
     </div>
   );
 }
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { BsInstagram, BsFacebook } from "react-icons/bs";
+import {AiOutlineTwitter} from "react-icons/ai"
+
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-light py-4 footer d-flex justify-content-center">
+            <Nav.Link href="#" className="fs-2 m-3">Sobre nosotros
+            <p className="fs-5 m-2">Nuestra Cultura</p>
+            <p className="fs-5 m-2">Nuestra mision</p>
+            <p className="fs-5 m-2">Historias y Novedades</p>
+            </Nav.Link>
+            <Nav.Link href="#" className="fs-2 m-3">Atencion al Cliente
+            <p className="fs-5 m-2">Contacto</p></Nav.Link>
+            <Nav.Link href="#" className="fs-2 m-3">Disfrutar Codeando?
+            <p className="fs-5 m-2">Tenemos la mejor experiencia para Ti</p></Nav.Link>
+        <p className="flex-end m-2">&copy; Todos los derechos reservados</p>
+        <BsInstagram className="m-2"/>
+        <BsFacebook className="m-2"></BsFacebook>
+        <AiOutlineTwitter className="m-2"   ></AiOutlineTwitter>
+       </footer>
+    );
+};
+
+export default Footer;
