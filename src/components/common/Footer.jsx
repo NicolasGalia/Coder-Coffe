808d2bb701ccc2884ca554e01e33616c561c2de4
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-11-11 11:29:48 -0300

    instalando dependencia de iconos

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { BsInstagram, BsFacebook } from "react-icons/bs";
 import {AiOutlineTwitter} from "react-icons/ai"
+import {MdContactPhone} from "react-icons/md"
+import {FaHistory} from "react-icons/fa"
 
 
 
@@ -11,10 +13,10 @@ const Footer = () => {
             <Nav.Link href="#" className="fs-2 m-3">Sobre nosotros
             <p className="fs-5 m-2">Nuestra Cultura</p>
             <p className="fs-5 m-2">Nuestra mision</p>
-            <p className="fs-5 m-2">Historias y Novedades</p>
+            <p className="fs-5 m-2"> <FaHistory></FaHistory>Historias y Novedades</p>
             </Nav.Link>
             <Nav.Link href="#" className="fs-2 m-3">Atencion al Cliente
-            <p className="fs-5 m-2">Contacto</p></Nav.Link>
+            <p className="fs-5 m-2"> <MdContactPhone></MdContactPhone> Contacto</p></Nav.Link>
             <Nav.Link href="#" className="fs-2 m-3">Disfrutar Codeando?
             <p className="fs-5 m-2">Tenemos la mejor experiencia para Ti</p></Nav.Link>
         <p className="flex-end m-2">&copy; Todos los derechos reservados</p>
