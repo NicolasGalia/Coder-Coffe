f928e9dfd818eb157df82844d90b193177e08360
EduRossB <edu_barba@outlook.com>
2022-11-26 02:03:20 -0300

    agregue estructura de pagina y agregue en paginacion

---
+++ src/views/PaginaPedido.jsx
@@ -0,0 +1,45 @@
+import "./css/pedido.css";
+import React from "react";
+import { Container, ListGroup, Row, Col, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import Swal from 'sweetalert2';
+
+const PaginaPedido = () => {
+  return (
+    <Container>
+      <h1 className="text-center mt-3 tituloPedido">Pedido</h1>
+      <Container className="containerPedido">
+        <ListGroup>
+          <h1 className="display-5 fs-5 text-center my-5">
+            Aún no agrego ningun producto a su pedido
+          </h1>
+
+          {/* {productosPedido.map((producto, posicion) => (
+                    <ItemProductoPedido
+                      key={posicion}
+                      nombreProducto={producto.nombreProducto}
+                      precio={producto.precio}
+                    ></ItemProductoPedido>
+                  ))} */}
+        </ListGroup>
+
+        <Row className="border-top border-dark bg-dark text-light rowTotal">
+          <Col className="fs-4 display-1 ps-5 my-1" xs={9}>
+            Total a pagar
+          </Col>
+          <Col className="fs-4 display-1 my-1">$</Col>
+        </Row>
+      </Container>
+      <div className="d-flex justify-content-evenly my-3">
+        <Button id="botonCancelarPedido" className="btn w-50 mx-3">
+          Eliminar todos los productos
+        </Button>
+        <Button id="botonEnviarPedido" className="btn w-50 mx-3">
+          Enviar pedido
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default PaginaPedido
