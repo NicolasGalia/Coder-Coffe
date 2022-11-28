9f8eab8429ed68e277a15c605dd9cfd0b0d38601
EduRossB <edu_barba@outlook.com>
2022-11-28 08:02:11 -0300

    cambio de base de dato a ls

--- src/views/PaginaPedido.jsx
+++ src/views/PaginaPedido.jsx
@@ -3,94 +3,67 @@ import React from "react";
 import { Container, ListGroup, Row, Col, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
-import {
-  actualizarPedidoUsuario,
-  consultarPedidoUsuario,
-  crearPedido,
-  eliminarProductoPedido,
-} from "../components/helpers/queriesPedido";
+import { crearPedido } from "../components/helpers/queriesPedido";
+import ItemProductoPedido from "./productoPedido/ItemProductoPedido";
 
 const PaginaPedido = () => {
-  const [usuario, setUsuario] = useState();
-  const [resultadoPedido, setResultadoPedido] = useState([]);
+
+  useEffect(() => {
+  
+  }, [])
+  
+
+  const listaProductosLocal =
+    JSON.parse(localStorage.getItem("listaProductos")) || [];
+  const [arregloProductos, setArregloProductos] = useState(listaProductosLocal);
+
+  const borrarProducto = (id) => {
+    let listaProductosActualizada = listaProductosLocal.filter(
+      (producto) => producto.id !== id
+    );
+    localStorage.setItem(
+      "listaProductos",
+      JSON.stringify(listaProductosActualizada)
+    );
+  };
+  const limpiarPedido = () => {
+    let listaProductosActualizada = [];
+    localStorage.setItem(
+      "listaProductos",
+      JSON.stringify(listaProductosActualizada)
+    );
+  };
 
   let preciosProductos = [];
 
-  for (let i = 0; i < usuario.productos.length; i++) {
-    preciosProductos = [...preciosProductos, usuario.productos[i].precio];
-  }
+  let resultadoPedido = 0;
 
-  const [totalPedido, setTotalPedido] = useState(0);
+  if (listaProductosLocal === []) {
+    for (let i = 0; i < listaProductosLocal.length; i++) {
+      preciosProductos = [...preciosProductos, listaProductosLocal[i].precio];
+    }
 
-  if (preciosProductos.length !== 0) {
-    const resultadoTotalPedido = preciosProductos.reduce((acc, item) => {
+    resultadoPedido = preciosProductos.reduce((acc, item) => {
       return (acc = acc + item);
     });
   }
-  
 
-  useEffect(() => {
-    // CONSULTAR DATOS DEL USUARIO INCLUYENDO DENTRO LOS PRODUCTOS DEL PEDIDO
-    consultarPedidoUsuario().then((pedido) => {
-      setUsuario(pedido);
-    });
-  }, [() => setTotalPedido(resultadoPedido)]);
+  // BASE DE DATOS FICTICIA
+  let listaNombreProductos = " ";
 
-  let nombre = usuario.nombre;
-  let apellido = usuario.apellido;
-  let email = usuario.email;
-  let userName = usuario.userName;
-  let password = usuario.password;
-  let pedido = usuario.pedido;
-
-  const eliminarUnProducto = (codigoUnico) => {
-    const productosActualizados = pedido.filter(
-      (producto) => producto._id !== codigoUnico
-    );
-    let productosNuevos = productosActualizados;
-    let usuarioActualizado = {
-      nombre: nombre,
-      apellido: apellido,
-      email: email,
-      userName: userName,
-      password: password,
-      pedido: productosNuevos,
-    };
-    actualizarPedidoUsuario(usuarioActualizado._id, usuarioActualizado);
-  };
-
-  const eliminarTodosProductos = () => {
-    let usuarioActualizado = {
-      nombre: nombre,
-      apellido: apellido,
-      email: email,
-      userName: userName,
-      password: password,
-      pedido: [],
-    };
-    actualizarPedidoUsuario(usuarioActualizado._id, usuarioActualizado);
-  };
-
-  const enviarPedido = () => {
-    if(totalPedido !== 0){
-    let pedidoParaEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      email: email,
-      productos: usuario.pedido,
-      total: totalPedido,
-      estado: "pendiente"
-    };
-    crearPedido(pedidoParaEnviar)
-  }
-  else{
-    Swal.fire(
-      'Error',
-      'Debe agregar productos al pedido.',
-      'error'
-    )
+  if (arregloProductos !== []) {
+    for (let i = 0; i < arregloProductos.length; i++) {
+      listaNombreProductos = +arregloProductos.nombreProducto;
+    }
   }
 
+  let pedidoFicticio = {
+    nombre: "Juan",
+    apellido: "Perez",
+    email: "juanperez@gmail.com",
+    productos: listaNombreProductos,
+    total: resultadoPedido,
+    estado: "pendiente",
   };
 
   return (
@@ -101,34 +74,37 @@ const PaginaPedido = () => {
           <h1 className="display-5 fs-5 text-center my-5">
             Aún no agrego ningun producto a su pedido
           </h1>
-
-          {/* {productos.map((producto) => (
-                    <ItemProductoPedido
-                      key={producto._id}
-                      nombreProducto={producto.nombreProducto}
-                      codigoUnico={producto._id}
-                      precio={producto.precio}
-                      eliminarUnProducto={eliminarUnProducto}
-                    ></ItemProductoPedido>
-                  ))} */}
+          {listaProductosLocal.map((producto) => (
+            <ItemProductoPedido
+              key={producto.id}
+              id={producto.id}
+              nombreProducto={producto.nombre}
+              precio={producto.precio}
+              borrarProductos={borrarProducto}
+            ></ItemProductoPedido>
+          ))}
         </ListGroup>
 
         <Row className="border-top border-dark bg-dark text-light rowTotal">
           <Col className="fs-4 display-1 ps-5 my-1" xs={9}>
             Total a pagar
           </Col>
-          <Col className="fs-4 display-1 my-1">${totalPedido}</Col>
+          <Col className="fs-4 display-1 my-1">${resultadoPedido}</Col>
         </Row>
       </Container>
       <div className="d-flex justify-content-evenly my-3">
         <Button
           id="botonCancelarPedido"
-          onClick={eliminarTodosProductos}
           className="btn w-50 mx-3"
+          onClick={limpiarPedido}
         >
           Eliminar todos los productos
         </Button>
-        <Button id="botonEnviarPedido" onClick={enviarPedido} className="btn w-50 mx-3">
+        <Button
+          id="botonEnviarPedido"
+          onClick={crearPedido(pedidoFicticio)}
+          className="btn w-50 mx-3"
+        >
           Enviar pedido
         </Button>
       </div>
--- src/views/productoPedido/ItemProductoPedido.jsx
+++ src/views/productoPedido/ItemProductoPedido.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { ListGroup, Button } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-const ItemProductoPedido = ({ nombreProducto, precio, eliminarUnProducto, codigoUnico }) => {
+const ItemProductoPedido = ({ nombreProducto, precio, id, borrarProductos}) => {
   return (
     <>
       <ListGroup.Item className="d-flex justify-content-between">
         <h1 className=" fs-5">{nombreProducto}</h1>
         <h2 className="fs-5 display-5">
           ${precio}
-          <Button className="bg-danger ms-3" onClick={()=>eliminarUnProducto(codigoUnico)}>
-            <i className=" text- light bi bi-trash3-fill"></i>
+          <Button className="bg-danger ms-3" onClick={borrarProductos(id)} >
+            <i className="text-light bi bi-trash3-fill"></i>
             
           </Button>
         </h2>
