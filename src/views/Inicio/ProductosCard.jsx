ff1522e2792c390a5f535204cf027aa414666423
florpalma1 <contr.lucascordoba98@gmail.com>
2022-12-05 22:13:23 -0300

    cambie las formas de las cards y agregue css nuevo

--- src/views/Inicio/ProductosCard.jsx
+++ src/views/Inicio/ProductosCard.jsx
@@ -11,11 +11,7 @@ const ProductosCard = ({producto}) => {
           <img src={producto.imagen} alt="producto"/>
           </div>
       <div className='content'>
-      <h1 className="tituloProducto m-3">{producto.nombreProducto}</h1>
-      <p className="descripcionProducto">
-        {producto.categoria}
-      </p>
-     <div className="precioProducto">${producto.precio}</div>
+      <h3 className="tituloProducto m-3">{producto.nombreProducto}</h3>
      <NavLink to="/error404" typeof='button' className="add">ver detalles</NavLink>
       </div>
       </div> 
