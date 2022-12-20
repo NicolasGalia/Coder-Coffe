a21f3fd53017a943f276ae4acbfdbee6856999ea
florpalma1 <contr.lucascordoba98@gmail.com>
2022-12-19 21:08:55 -0300

    agregue banner con codigo qr para desplegar menu en pdf

--- src/views/Inicio/Menu.jsx
+++ src/views/Inicio/Menu.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logoBgTransparent from "../../img/logoBgTransparent.png"
+
+
 
 
 const Menu = () => {
-    return (
-
-      <div>hola</div>
-    );
+  return (
+    <div  class="carousel slide mb-5">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://i.postimg.cc/4N2V3k5S/Agregar-un-t-tulo-1.png" class="d-block w-100" alt="banner menu"/>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Menu;
